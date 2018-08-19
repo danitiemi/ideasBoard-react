8da8ac27d7c3444eba7ad9c3b898f7f0b172315d
danitiemi <danitiemi.br@gmail.com>
2018-08-19 11:09:15 -0700

    Immutability helper and a method to add new ideas

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -3,9 +3,21 @@ module Api::V1
     class IdeasController < ApplicationController
 
         def index
-            @ideas = Idea.all
+            @ideas = Idea.order("created_at DESC")
             render json: @ideas
         end
+
+        # API endpoint for creating new ideas 
+        def create
+            @idea = Idea.create(idea_params)
+            render json: @idea
+        end
+          
+        private
+          
+            def idea_params
+              params.require(:idea).permit(:title, :body)
+            end
     end
 
 end
--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Idea from './Idea'
 import axios from 'axios'
+import update from 'immutability-helper'
 
 class IdeasContainer extends Component {
   
@@ -8,7 +9,8 @@ class IdeasContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          ideas: []
+          ideas: [],
+          editingIdeaId: null
         }
     }
   
@@ -19,13 +21,39 @@ class IdeasContainer extends Component {
           this.setState({ideas: response.data})
         })
         .catch(error => console.log(error))
-      }
+    }
+
+    //  make a new copy of this.state.ideas and 
+    // $splice command insert the new idea in response.data, at the 0th index of this array.
+    addNewIdea = () => {
+        axios.post(
+          'http://localhost:3001/api/v1/ideas',
+          { idea:
+            {
+              title: '',
+              body: ''
+            }
+          }
+        )
+        .then(response => {
+            console.log(response)
+            const ideas = update(this.state.ideas, {
+                $splice: [[0, 0, response.data]]
+            })
+            this.setState({
+                ideas: ideas,
+                editingIdeaId: response.data.id
+            })
+        })
+        .catch(error => console.log(error))
+    }
   
     render() {
     return (
       <div>
         <div>
-          <button className="newIdeaButton">
+          <button className="newIdeaButton"
+            onClick={this.addNewIdea}>
             New Idea
           </button>
         </div>
