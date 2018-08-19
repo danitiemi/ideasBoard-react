800d330167e48cc356c12b45039a2a296cdf0327
danitiemi <danitiemi.br@gmail.com>
2018-08-19 13:07:25 -0700

    Feature: delete ideas

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -75,6 +75,17 @@ class IdeasContainer extends Component {
             () => { this.title.focus() })
     }
 
+    deleteIdea = (id) => {
+        axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+        .then(response => {
+          const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+          const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+          this.setState({ideas: ideas})
+        })
+        .catch(error => console.log(error))
+    }
+      
+
     render() {
         return (
             <div>
@@ -95,7 +106,8 @@ class IdeasContainer extends Component {
                             resetNotification={this.resetNotification} />)
                     } else {
                         return (<Idea idea={idea} key={idea.id}
-                            onClick={this.enableEditing} />)
+                            onClick={this.enableEditing}
+                            onDelete={this.deleteIdea} />)
                     }    
                 })}
             </div>
